3a0344681ca12ff22d7191edbf74d9182360aecc
marcelslum <marcelslum@yahoo.com.br>
2016-08-19 18:13:10 -0300

    Update TutorialLoader.java

--- app/src/main/java/ultno/marcelslum/ultnogame/TutorialLoader.java
+++ app/src/main/java/ultno/marcelslum/ultnogame/TutorialLoader.java
@@ -15,11 +15,37 @@ public class TutorialLoader {
 
     public static void loadTutorial(Game game, int levelNumber){
     
+    Game innerGame = game;
+
     switch (levelNumber){
             case 1:
-
-                TextBox tb = new TextBox("textBox", game, 50f, 50f, game.resolutionX*0.6f, game.resolutionX*0.03f, "Atinja o alvo com a bola para destruir o alvo que desaparecerá após ser atingido!!!");
-                game.levelObject.tutorials.add(new Tutorial(tb));
+                TextBox l1tb1 = new TextBox("textBox", game, 50f, 50f, game.resolutionX*0.6f, game.resolutionX*0.03f, game.context.getResources().getString(R.string.l1t1));
+                Tutorial l1t1 = new Tutorial(l1tb1);
+                l1t1.setOnShowBeforeAnim(new Tutorial.OnShowBeforeAnim() {
+                    @Override
+                    public void onShowBeforeAnim() {
+                        innerGame.balls.get(0).clearDisplay();
+                        innerGame.bars.get(0).isMovable = true;
+                    }
+                });
+                game.levelObject.tutorials.add(l1t1);
+                
+                TextBox l1tb2 = new TextBox("textBox", game, 50f, 50f, game.resolutionX*0.6f, game.resolutionX*0.03f, game.context.getResources().getString(R.string.l1t2));
+                Tutorial l1t2 = new Tutorial(l1tb2);
+                l1t2.setOnShowBeforeAnim(new Tutorial.OnShowBeforeAnim() {
+                    @Override
+                    public void onShowBeforeAnim() {
+                        innerGame.balls.get(0).display();
+                        innerGame.bars.get(0).isMovable = false;
+                        innerGame.bars.get(0).x = innerGame.levelObject.barsInitialX[0]];
+                        innerGame.bars.get(0).y = innerGame.levelObject.barsInitialY[0]];
+                    }
+                });
+                game.levelObject.tutorials.add(l1t2);
+                
+                
+                
+                
                 break;
                 
             case 2:
