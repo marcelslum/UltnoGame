b5172aa967b2ae8873dffa4b11a3680a614bcbcf
marcelslum <marcelslum@yahoo.com.br>
2016-08-30 15:46:05 -0300

    Update WindowGame.java

--- app/src/main/java/ultno/marcelslum/ultnogame/WindowGame.java
+++ app/src/main/java/ultno/marcelslum/ultnogame/WindowGame.java
@@ -4,8 +4,31 @@ package ultno.marcelslum.ultnogame;
  * Created by marcel on 26/08/2016.
  */
 public class WindowGame extends Entity{
+    
+    int quantityOfLines;
+    float distance;
+    float height;
+    float size;
+    float [] linesY;
+    
+    
+    
 
-    public WindowGame(String name, Game game, float x, float y) {
+    public WindowGame(String name, Game game, float x, float y, int quantityOfLines, float height, float distance, ) {
         super(name, game, x, y);
+        this.quantityOfLines = quantityOfLines;
+        this.distance = distance;
+        this.height = height;
+        
+        
+        this.size = height/(quantityOfLines - (distance * quantityOfLines) + distance);
+        
+        linesY = new float[quantityOfLines];
+        for (int i = 0; i < quantityOfLines; i++){
+            linesY[i] = i * (size * (1 - distance)); 
+            
+            
+        }
+        
     }
 }
