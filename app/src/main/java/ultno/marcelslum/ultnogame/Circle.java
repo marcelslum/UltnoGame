7df92d3657f576edfa1f5f390509f29f7b43aff1
marcelslum <marcelslum@yahoo.com.br>
2016-08-31 11:37:37 -0300

    Update Circle.java

--- app/src/main/java/ultno/marcelslum/ultnogame/Circle.java
+++ app/src/main/java/ultno/marcelslum/ultnogame/Circle.java
@@ -11,39 +11,48 @@ public class Circle extends PhysicalObject {
     Circle(String name, Game game, float x, float y, float radius, int weight){
         super(name, game, x, y, weight);
         this.radius = radius;
-
     }
 
     @Override
     public void updateQuatreeData() {
-        this.quadtreeData.setX(x - radius);
-        this.quadtreeData.setY(y - radius);
-        this.quadtreeData.setWidth(radius *2);
-        this.quadtreeData.setHeight(radius *2);
+        float size = getTransformedWidht();
+        this.quadtreeData.setX(positionX - size/2f);
+        this.quadtreeData.setY(positionY - size/2f);
+        this.quadtreeData.setWidth(size);
+        this.quadtreeData.setHeight(size);
     }
 
     @Override
     public void setSatData(){
-        if(this.circleData == null) {
+        if(circleData == null) {
             //Log.e("setSatData", "setando data no circle");
             //Log.e("setSatData", "radius "+this.radius);
-            this.circleData = new SatCircle(new Vector(this.x, this.y), this.radius);
+            circleData = new SatCircle(new Vector(positionX, positionY), radius);
         } else {
-            this.circleData.pos.x = this.x;
-            this.circleData.pos.y = this.y;
-            this.circleData.r = this.radius;
+            circleData.pos.x = positionX;
+            circleData.pos.y = positionY;
+            circleData.r = radius;
         }
     }
 
     @Override
-    public float getMiddlePointX() {
-        return this.radius;
-
+    public float getWidth() {
+        return radius*2f;
     }
 
     @Override
-    public float getMiddlePointY() {
-        return this.radius;
+    public float getHeight() {
+        return radius*2f;
+    }
+    
+    @Override
+    public float getTransformedWidth() {
+        return (radius*2f)*accumulatedScaleX;
+    }
+
+    @Override
+    public float getTransformedHeight() {
+        return (radius*2f)*accumulatedScaleX;
     }
 
 }
