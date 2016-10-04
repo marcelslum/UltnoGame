3f9c0fd516c9b182f1e03da3e0788ec78802288d
marcelslum <marcelslum@yahoo.com.br>
2016-10-04 16:30:54 -0300

    Update InitLoader.java

--- app/src/main/java/ultno/marcelslum/ultnogame/InitLoader.java
+++ app/src/main/java/ultno/marcelslum/ultnogame/InitLoader.java
@@ -1,31 +1,28 @@
-private class InitLoader extends AsyncTask<URL, Integer, Long> {
-      protected Long doInBackground(URL... urls) {
-         
-      Game.initData();
-      Storage.initializeStorage(context, quantityOfLevels);
-      Game.maxLevel = Storage.getMaxLevel();
-      Game.difficulty = Storage.getDificulty();
-      Game.changeDifficulty(difficulty);
-      Game.levelNumber = Storage.getActualLevel();
-      Game.initTime = Utils.getTime();
-      Game.initTextures();
-      Sound.init();
-      Game.initFont();
-      Game.initMenus();
-      Game.initTexts();
-      Game.initEdges();
-      Game.frame = new Rectangle("frame", 0f, 0f, resolutionX, resolutionY, -1, new Color(0f, 0f, 0f, 1f));
-      Game.frame.clearDisplay();
-      Game.frame.alpha = 0f;
-
-      return totalSize;
+private class InitLoader extends AsyncTask<void, void, void> {
+      protected void doInBackground() {
+                  
+            Game.initData();
+            Storage.initializeStorage(context, quantityOfLevels);
+            Game.maxLevel = Storage.getMaxLevel();
+            Game.difficulty = Storage.getDificulty();
+            Game.changeDifficulty(difficulty);
+            Game.levelNumber = Storage.getActualLevel();
+            Game.initTime = Utils.getTime();
+            Game.initTextures();
+            Sound.init();
+            Game.initFont();
+            Game.initMenus();
+            Game.initTexts();
+            Game.initEdges();
+            Game.frame = new Rectangle("frame", 0f, 0f, resolutionX, resolutionY, -1, new Color(0f, 0f, 0f, 1f));
+            Game.frame.clearDisplay();
+            Game.frame.alpha = 0f;
      }
 
-     protected void onProgressUpdate(Integer... progress) {
-         setProgressPercent(progress[0]);
+     protected void onProgressUpdate() {
      }
 
-     protected void onPostExecute(Long result) {
-         showDialog("Downloaded " + result + " bytes");
+     protected void onPostExecute() {
+        Game.loaderConclude = true;
      }
  }
