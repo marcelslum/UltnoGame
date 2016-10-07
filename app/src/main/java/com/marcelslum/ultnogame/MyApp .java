989f842a2573a938034c07ab021b65a312f37ba9
marcelslum <marcelslum@yahoo.com.br>
2016-10-07 17:08:08 -0300

    Update MyApp .java

--- app/src/main/java/com/marcelslum/ultnogame/MyApp .java
+++ app/src/main/java/com/marcelslum/ultnogame/MyApp .java
@@ -2,9 +2,6 @@ public final class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FontsOverride.setDefaultFont(this, "DEFAULT", "jetset.ttf");
-        FontsOverride.setDefaultFont(this, "MONOSPACE", "jetset.ttf");
         FontsOverride.setDefaultFont(this, "SERIF", "jetset.ttf");
-        FontsOverride.setDefaultFont(this, "SANS_SERIF", "jetset.ttf");
     }
 }
