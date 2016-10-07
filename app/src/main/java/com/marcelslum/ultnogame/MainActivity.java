e29efd2650646e7018df4043c1519dee75ae6133
marcelslum <marcelslum@yahoo.com.br>
2016-10-07 15:11:16 -0300

    Update MainActivity.java

--- app/src/main/java/com/marcelslum/ultnogame/MainActivity.java
+++ app/src/main/java/com/marcelslum/ultnogame/MainActivity.java
@@ -18,12 +18,93 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnConnectionFailedListener{
 
     // Our OpenGL Surfaceview
     private GLSurfaceView glSurfaceView;
     private InterstitialAd interstitial;
 
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+	
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+	if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+	    // Show dialog using GoogleApiAvailability.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
+            //TODO informar erro ao usuario
+            
+        }
+    }
+	
+
+	// The rest of this code is all about building the error dialog
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+	/* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(), errorCode, REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MyActivity) getActivity()).onDialogDismissed();
+        }
+    }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_RESOLVE_ERROR) {
+		mResolvingError = false;
+		if (resultCode == RESULT_OK) {
+		    // Make sure the app is not already connected or attempting to connect
+		    if (!mGoogleApiClient.isConnecting() &&
+			    !mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.connect();
+		    }
+		}
+	    }
+	}
+
+
+
+	
+	
 
 
     @Override
@@ -65,6 +146,15 @@ public class MainActivity extends Activity {
 	    
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	    
+	GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+	    .enableAutoManage(this /* FragmentActivity */,
+			      this /* OnConnectionFailedListener */)
+	    .addApi(Drive.API)
+	    .addScope(Drive.SCOPE_FILE)
+	    .build();
+	    
+	    
 
         //Game.mGoogleApiClient = new GoogleApiClient.Builder(this)
         //        .addConnectionCallbacks(this)
