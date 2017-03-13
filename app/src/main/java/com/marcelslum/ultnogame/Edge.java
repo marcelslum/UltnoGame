9237a3ce4ecb109a2504b95c2640128d2deef32e
marcelslum <marcelslum@yahoo.com.br>
2017-03-13 15:34:39 -0300

    Update Edge.java

--- app/src/main/java/com/marcelslum/ultnogame/Edge.java
+++ app/src/main/java/com/marcelslum/ultnogame/Edge.java
@@ -4,8 +4,8 @@ package com.marcelslum.ultnogame;
  * Created by marcel on 27/09/2016.
  */
 public class Edge extends Rectangle{
-    Edge(String name, float x, float y, float width, float height) {
-        super(name, x, y, width, height, Game.BORDA_WEIGHT, new Color(0,0,0,1));
+    Edge(String name, float x, float y, int type, float width, float height) {
+        super(name, x, y, type, width, height, Game.BORDA_WEIGHT, new Color(0,0,0,1));
         isMovable = false;
         isVisible = true;
         isCollidable = false;
