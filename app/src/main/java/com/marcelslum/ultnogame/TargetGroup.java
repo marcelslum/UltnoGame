ce96e2013ede742567da22ee284fb163b16f051e
marcelslum <marcelslum@yahoo.com.br>
2017-03-16 18:05:17 -0300

    Update TargetGroup.java

--- app/src/main/java/com/marcelslum/ultnogame/TargetGroup.java
+++ app/src/main/java/com/marcelslum/ultnogame/TargetGroup.java
@@ -31,25 +31,4 @@ public ArrayList<TargetGroupData> targets;
             uvsBuffer = Utils.generateFloatBuffer(uvsData);
         }
   }
-  
-  private class TargetGroupData{
-      int id;
-      float x;
-      float y;
-      float width;
-      float height;
-      float alpha;
-      int type;
-    
-      TargetGroupData(int id, float x, float y, float width, float height, float alpha, int type){
-          this.id = id;
-          this.x = x;
-          this.y = y;
-          this.width = width;
-          this.height = height;
-          this.alpha = alpha;
-          this.type = type;
-      }
-  }
-
 }
