3f57e0ce95d94f4a40cced40350249a1d7fae504
marcelslum <marcelslum@yahoo.com.br>
2017-03-16 17:50:42 -0300

    Create TargetGroupData.java

---
+++ app/src/main/java/com/marcelslum/ultnogame/TargetGroupData.java
@@ -0,0 +1,19 @@
+  public class TargetGroupData{
+      int id;
+      float x;
+      float y;
+      float width;
+      float height;
+      float alpha;
+      int type;
+    
+      TargetGroupData(int id, float x, float y, float width, float height, float alpha, int type){
+          this.id = id;
+          this.x = x;
+          this.y = y;
+          this.width = width;
+          this.height = height;
+          this.alpha = alpha;
+          this.type = type;
+      }
+  }
