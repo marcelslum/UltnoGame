3e28325ca97ba557cca4847f046a1e5487b1bb0a
marcelslum <marcelslum@yahoo.com.br>
2017-03-23 17:42:41 -0300

    Update DataBaseContract.java

--- app/src/main/java/com/marcelslum/ultnogame/DataBaseContract.java
+++ app/src/main/java/com/marcelslum/ultnogame/DataBaseContract.java
@@ -3,7 +3,6 @@ public final class DataBaseContract {
     }
 
     public static class Balls implements BaseColumns {
-    
         public static final String TABLE_NAME = "balls";
         public static final String COLUMN_LEVEL = "level";
         public static final String COLUMN_RADIUS = "radius";
@@ -21,4 +20,26 @@ public final class DataBaseContract {
         public static final String COLUMN_MIN_VELOCITY = "minVelocity";
         public static final String COLUMN_FREE = "free";
     }
+    
+    public static class Bars implements BaseColumns {
+        public static final String TABLE_NAME = "bars";
+        public static final String COLUMN_LEVEL = "level";
+        public static final String COLUMN_WIDTH = "width";
+        public static final String COLUMN_HEIGHT = "height";
+        public static final String COLUMN_X = "x";
+        public static final String COLUMN_Y = "y";
+        public static final String COLUMN_VX = "vx";
+    }
+    
+    public static class Targets implements BaseColumns {
+        public static final String TABLE_NAME = "targets";
+        public static final String COLUMN_WIDTH = "width";
+        public static final String COLUMN_HEIGHT = "height";
+        public static final String COLUMN_DISTANCE = "distance";
+        public static final String COLUMN_PADD = "padd";
+        
+       
+    }
+    
+    
 }
