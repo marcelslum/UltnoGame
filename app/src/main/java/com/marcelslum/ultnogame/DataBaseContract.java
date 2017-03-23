974d81e9750996d6d8bcab9efd1e9b79e3ed71bf
marcelslum <marcelslum@yahoo.com.br>
2017-03-23 17:39:16 -0300

    Create DataBaseContract.java

---
+++ app/src/main/java/com/marcelslum/ultnogame/DataBaseContract.java
@@ -0,0 +1,24 @@
+public final class DataBaseContract {
+    private DataBaseContract() {
+    }
+
+    public static class Balls implements BaseColumns {
+    
+        public static final String TABLE_NAME = "balls";
+        public static final String COLUMN_LEVEL = "level";
+        public static final String COLUMN_RADIUS = "radius";
+        public static final String COLUMN_X = "x";
+        public static final String COLUMN_Y = "y";
+        public static final String COLUMN_VX = "vx";
+        public static final String COLUMN_VY = "vy";
+        public static final String COLUMN_TEXTURE_MAP = "textureMap";
+        public static final String COLUMN_INVENCIBLE = "invencible";
+        public static final String COLUMN_ANGLE_TO_ROTATE = "angleToRotate";
+        public static final String COLUMN_MAX_AGLE = "maxAngle";
+        public static final String COLUMN_MIN_ANGLE = "minAngle";
+        public static final String COLUMN_VELOCITY_VARIATION = "velocityVariation";
+        public static final String COLUMN_MAX_VELOCITY = "maxVelocity";
+        public static final String COLUMN_MIN_VELOCITY = "minVelocity";
+        public static final String COLUMN_FREE = "free";
+    }
+}
