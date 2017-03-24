d5a991daf3625784dd0a11c4b0f5e3cf4c1df616
marcelslum <marcelslum@yahoo.com.br>
2017-03-24 16:11:53 -0300

    Update TargetDataBaseData.java

--- app/src/main/java/com/marcelslum/ultnogame/TargetDataBaseData.java
+++ app/src/main/java/com/marcelslum/ultnogame/TargetDataBaseData.java
@@ -3,4 +3,12 @@ public class TargetDataBaseData{
     public float height;
     public float distance;
     public float padd;
+    public final static String TAG = "TargetDataBaseData";
+    
+    public void logData(){
+        Log.e(TAG, "width "+width);   
+        Log.e(TAG, "height "+height);   
+        Log.e(TAG, "distance "+distance);   
+        Log.e(TAG, "padd "+padd);    
+    }
 }
