3448bd2b1f4f2f6c06c584d732419d935f91e37a
marcelslum <marcelslum@yahoo.com.br>
2017-03-24 16:09:51 -0300

    Update BallDataBaseData.java

--- app/src/main/java/com/marcelslum/ultnogame/BallDataBaseData.java
+++ app/src/main/java/com/marcelslum/ultnogame/BallDataBaseData.java
@@ -17,5 +17,24 @@ public class BallDataBaseData{
     public float minVelocity;
     public int free;
     
+    public final static String TAG = "BallDataBaseData";
+    
+    public void logData(){
+        Log.e(TAG, "radius "+radius);   
+        Log.e(TAG, "x "+x);   
+        Log.e(TAG, "y "+y);   
+        Log.e(TAG, "vx "+vx);   
+        Log.e(TAG, "vy "+vy);   
+        Log.e(TAG, "textureMap "+textureMap);   
+        Log.e(TAG, "invencible "+invencible);   
+        Log.e(TAG, "angleToRotate "+angleToRotate);   
+        Log.e(TAG, "maxAngle "+maxAngle);   
+        Log.e(TAG, "minAngle "+minAngle);   
+        Log.e(TAG, "velocityVariation "+velocityVariation);   
+        Log.e(TAG, "maxVelocity "+maxVelocity);   
+        Log.e(TAG, "minVelocity "+minVelocity);   
+        Log.e(TAG, "free "+free);   
+    }
+    
     
 }
