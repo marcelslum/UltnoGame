e1cbc2608dbac03ba523722eab27de4e9e108891
marcelslum <marcelslum@yahoo.com.br>
2017-03-24 16:10:48 -0300

    Update BarDataBaseData.java

--- app/src/main/java/com/marcelslum/ultnogame/BarDataBaseData.java
+++ app/src/main/java/com/marcelslum/ultnogame/BarDataBaseData.java
@@ -3,5 +3,14 @@ public class BarDataBaseData{
     public float height;
     public float x;
     public float y;
-    public float vx;    
+    public float vx;
+    public final static String TAG = "BarDataBaseData";
+    
+    public void logData(){
+        Log.e(TAG, "width "+width);   
+        Log.e(TAG, "height "+height);   
+        Log.e(TAG, "x "+x);   
+        Log.e(TAG, "y "+y);   
+        Log.e(TAG, "vx "+vx);   
+    }
 }
