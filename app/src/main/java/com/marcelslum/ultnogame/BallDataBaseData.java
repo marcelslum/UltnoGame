3ff71812bc94c0f4b8f7a9c009816fa66ef76c9b
marcelslum <marcelslum@yahoo.com.br>
2017-03-24 15:41:53 -0300

    Create BallDataBaseData.java

---
+++ app/src/main/java/com/marcelslum/ultnogame/BallDataBaseData.java
@@ -0,0 +1,21 @@
+package com.marcelslum.ultnogame;
+
+public class BallDataBaseData{
+
+    public float radius;
+    public float x;
+    public float y;
+    public float vx;
+    public float vy;
+    public int textureMap;
+    public int invencible;
+    public int angleToRotate;
+    public int maxAngle;
+    public int minAngle;
+    public float velocityVariation;
+    public float maxVelocity;
+    public float minVelocity;
+    public int free;
+    
+    
+}
