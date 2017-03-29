337b4dc5d7eb77382ef22c95fd45a08eb35bb183
marcelslum <marcelslum@yahoo.com.br>
2017-03-29 15:42:54 -0300

    Create shader_frag_vertex_e_uv_com_alpha.glsl

---
+++ app/src/main/res/raw/shader_frag_vertex_e_uv_com_alpha.glsl
@@ -0,0 +1,9 @@
+precision mediump float;
+varying float alpha;
+varying vec2 vv2_texCoord;
+uniform sampler2D us_texture;
+void main() {
+   vec4 color = texture2D( us_texture, vv2_texCoord);
+   if (color.a < 0.001) discard;
+   gl_FragColor = vec4(color.r, color.g, color.b, color.a * alpha);
+}
