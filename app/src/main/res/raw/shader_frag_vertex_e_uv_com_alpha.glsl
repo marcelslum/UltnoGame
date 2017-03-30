e1e221676905560f8e6adb6e9ad818792dbd51ec
marcelslum <marcelslum@yahoo.com.br>
2017-03-30 15:15:09 -0300

    Update shader_frag_vertex_e_uv_com_alpha.glsl

--- app/src/main/res/raw/shader_frag_vertex_e_uv_com_alpha.glsl
+++ app/src/main/res/raw/shader_frag_vertex_e_uv_com_alpha.glsl
@@ -1,11 +1,10 @@
 precision mediump float;
 varying float alpha;
+uniform float uf_alpha;
 varying vec2 vv2_texCoord;
 uniform sampler2D us_texture;
 void main() {
    vec4 color = texture2D( us_texture, vv2_texCoord);
    if (color.a < 0.001) discard;
-   gl_FragColor = vec4(color.r, color.g, color.b, color.a * alpha * 0.5);
-   //gl_FragColor = vec4(0.8, 0.2, 0.9, 1.0);
-   //gl_FragColor = vec4(color.r, color.g, color.b, 1.0);
+   gl_FragColor = vec4(color.r, color.g, color.b, color.a * alpha * 0.5 * uf_alpha);
 }
