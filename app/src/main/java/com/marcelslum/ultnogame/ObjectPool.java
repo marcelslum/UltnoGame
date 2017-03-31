5c2a3a18ece75bac744e36236adbdc37ded2e941
marcelslum <marcelslum@yahoo.com.br>
2017-03-31 17:11:03 -0300

    Update ObjectPool.java

--- app/src/main/java/com/marcelslum/ultnogame/ObjectPool.java
+++ app/src/main/java/com/marcelslum/ultnogame/ObjectPool.java
@@ -34,7 +34,11 @@ public class ObjectPool<Type> implements Pool<Type> {
 
             //Log.e("ObjectPool", "freeObjectsID " + freeObjectsID.size());
 
-            final Poolable<Type> obj = (Poolable<Type>) factory.newObject();
+            if (factory != null){
+                final Poolable<Type> obj = (Poolable<Type>) factory.newObject();
+            } else {
+                final Poolable<Type> obj = (Poolable<Type>) new Poolable<Type>();//// ver se funciona
+            }
             obj.setPoolID(objects.size());
 
             //Log.e("ObjectPool", "objects " + objects.size());
