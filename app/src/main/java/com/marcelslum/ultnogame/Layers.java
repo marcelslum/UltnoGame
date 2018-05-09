07abe02dbcd7681268fb766e80ebf25b0397d0ed
marcelslum <marcelslum@gmail.com>
2018-05-09 16:16:33 -0300

    Create Layers.java

---
+++ app/src/main/java/com/marcelslum/ultnogame/Layers.java
@@ -0,0 +1,14 @@
+public class Layers{
+
+public static final int LAYER1 = 1;
+public static final int LAYER2 = 2;
+public static final int LAYER3 = 3;
+public static final int LAYER4 = 4;
+public static final int LAYER5 = 5;
+public static final int LAYER6 = 6;
+public static final int LAYER7 = 7;
+public static final int LAYER8 = 8;
+public static final int LAYER9 = 9;
+public static final int LAYER10 = 10;
+
+}
